a6c033f93e982b2296c00a7bd28ff671130bc795
Ondrej Barci <ondrej.barci@ferratum.com>
2020-01-12 16:25:02 +0100

    adapt to heroku hosting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import {environment} from '../environments/environment';
 const config = {
   clientId: '0oa2dy85fyiB7VD9u357',
   issuer: 'https://dev-923407.okta.com/oauth2/default',
-  redirectUri: environment.redirectUri,
+  redirectUri: 'https://goal-tracker-fe.herokuapp.com/implicit/callback',
   scopes: ['openid', 'profile', 'email'],
   pkce: true
 };
